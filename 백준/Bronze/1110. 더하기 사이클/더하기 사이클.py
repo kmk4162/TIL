653750426a4cdf4fb5974c6d6b641ed049c4b86f
MunGyeong Kim <108652903+kmk4162@users.noreply.github.com>
2022-07-26 01:02:03 +0900

    Update 더하기 사이클.py

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,15 +1,20 @@
-N = int(input()) # 얘는 고정, 비교를 계속 해야하니까
-num = N
+N = int(input())
+
 cnt = 0
+new_number = N
 
 while True:
-    A = num // 10
-    B = num % 10
-    var_1 = A + B
-    var_2 = num % 10
-    var_3 = var_1 % 10
-    num = var_2 * 10 + var_3
     cnt += 1
-    if num == N:
+    tens = new_number // 10
+    units = new_number % 10
+    new_number = tens + units # 각 자리수 덧셈해서 새로운 숫자 생성
+    new_number_units = new_number % 10 
+    final_number = units * 10 + new_number_units
+    # 이전 단계의 일의 자리 숫자를 십의자리로 하고,
+    # new_number의 일의 자리 숫자를 일의자리로 쓰는 숫자로 업데이트!
+    if N == final_number:
+        print(cnt)
         break
-print(cnt)
+    else:
+        new_number = final_number
+
