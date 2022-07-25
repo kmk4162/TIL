aa084ea4f8179196fa829b5b9808e1c5b59c19a5
MunGyeong Kim <108652903+kmk4162@users.noreply.github.com>
2022-07-25 14:40:44 +0900

    Update 별 찍기 － 2.py

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -3,3 +3,10 @@ N = int(input())
 for i in range(1, N + 1):
     print(' ' * (N - i) + '*' * i)
     i += 1
+
+#----------------------------------
+# star = int(input())
+
+# for i in range(1, star + 1):
+#     print(' ' * (star - i) , end = '')
+#     print('*' * i)
