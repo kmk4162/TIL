619b396a9991b1ffa28a9492414eae59741dda01
MunGyeong Kim <108652903+kmk4162@users.noreply.github.com>
2022-07-25 14:07:36 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,13 +1,9 @@
 A = int(input())
 B = int(input())
 C = int(input())
-result = A * B * C
-r_list = str(result)
-#* 문자열로 변환 후 list를 하면 자리 하나하나 별로 요소가 나뉘어짐!
-num_dict = {}
-
-for i in r_list:
-    num_dict[int(i)] = num_dict.get(int(i), 0) + 1
-#* 딕셔너리에 횟수 저장
-for j in range(0, 10):
-    print(num_dict.get(j , 0))
+result = str(A * B * C)
+num_list = [0] * 10
+for num in result:
+    num_list[int(num)] += 1
+for i in num_list:
+    print(i)
