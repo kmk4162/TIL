3c28c275943be980a90c7235ef01d2f2b339735f
MunGyeong Kim <108652903+kmk4162@users.noreply.github.com>
2022-07-26 17:05:58 +0900

    Update 분해합.py

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -17,3 +17,18 @@ while result < N:
         continue
 if bool(gen_list) == False:
     print(0)
+
+#* 검색해서 찾아본 코드
+N = int(input())
+result = 0
+
+for i in range(1, N + 1):
+    digits = list(map(int, str(i)))
+    generator = sum(digits) + i
+    if generator == N:
+        result = i
+        break
+print(result)
+# result값이 0이고 따로 업데이트 안 하면 그대로 0을 출력
+# result값이 업데이트가 안 된다는 뜻은 조건에 맞는 generator가
+# 없음을 의미하기 때문에 좀 더 깔끔하게 코드 작성 가능
