70d4eac7b333fbcc2e01222ddbbc155710609bb0
kmk4162 <kmk41623@gmail.com>
2022-07-29 09:34:49 +0900

    Update 1292.py

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -1,3 +1,4 @@
+# 내 풀이
 start, end = map(int, input().split())
 list1 = []
 
@@ -8,7 +9,26 @@ def seq(a):
     return list1
 seq(50)
 x = sum(list1[start - 1 : end])
+print(x)
+
 # 실제 순서는 1 2 3 4 ...지만
 # 파이썬의 index는 0 1 2 3 4기 때문에
 # start -1 처리, end는 어차피 미만까지니까 그대로
-print(x)
+
+#! -----------------------------------------------------------
+
+#* 강사님 풀이 
+
+# 수열의 길이가 B보다만 크면 됨(어차피 B가지의 합만 구하니까)
+# 수열의 길이가 B보다 작을때 까지만 반복
+#? 범위가 명확하면 for, 애매하면 while이 더 좋음 
+
+# start, end = map(int, input().split())
+# list1 = []
+# N = 4
+
+# while len(list1) < end:
+#     for i in range(N):
+#         list1.append(N)
+#     N += 1
+
