494389fe5d81fa9c9b718f7e2e2051de98bbca81
kmk4162 <kmk41623@gmail.com>
2022-08-02 09:35:56 +0900

    Update 23253.py

--- 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.py
+++ 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.py
@@ -22,3 +22,22 @@ else:
 # 따라서 순서대로 책 꺼내기가 가능하다면 sort로 정렬해도 기존 순서랑
 # 동일해야 하지만, 만약 순서가 바뀐다면 애초부터 내림차순 정렬이 아니였다는
 # 뜻이기 때문에 순서대로 책을 꺼내기가 불가능함
+
+#* 스택 사용법
+
+# 맨 위(top)위치의 값을 pop()하고 그전의 인덱스의 값과
+# 계속 비교하는 방식
+
+for stack in stacklist:
+    comparison = stack.pop()
+    answer = 'Yes' # 미리 Yes를 디폴트로 정해둠
+
+    while len(stack) != 0:
+        if stack[-1] > comparison: 
+            comparison = stack.pop()  
+        else:
+            answer = 'No'
+            break
+print(answer)
+
+
