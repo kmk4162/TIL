54fe7ad396b7dff2406a6dadfbf86005ebea05d0
MunGyeong Kim <108652903+kmk4162@users.noreply.github.com>
2022-08-30 00:58:37 +0900

    Update A → B.py
    BFS 풀이 추가

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -21,3 +21,25 @@ if A != B:
     print(-1)
 else:
     print(cnt)
+ 
+# BFS 풀이
+from collections import deque
+
+A, B = map(int, input().split())
+# A에서 B까지
+# 연산 최솟값에 +1 출력, 못 만들면 -1
+# 100, 40021 경우
+
+def bfs(A, B):
+    queue = deque([(A, 1)])
+    while queue:
+        num, cnt = queue.popleft()
+        if num == B:
+            print(cnt)
+            return
+
+        for i in [num * 2, num * 10 + 1]:
+            if 1 <= i <= 1e9 and A <= i:
+                queue.append((i, cnt+1))
+    print(-1)
+bfs(A, B)
