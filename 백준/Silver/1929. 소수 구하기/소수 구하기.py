38721a281aa6e63508c56dcf55659246af4ca8b0
MunGyeong Kim <108652903+kmk4162@users.noreply.github.com>
2022-11-14 01:34:10 +0900

    [Silver III] Title: 소수 구하기, Time: 360 ms, Memory: 38512 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -2,17 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = [True] * m
+arr = [True] * (m + 1)
 root = int(m ** 0.5)
 
-def isPrime(x):
-    if x == 1:
-        return False
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0:
-            return False
-    return True
+for i in range(2, root + 1):
+    if arr[i] == True:
+        # 배수 만큼
+        for j in range(i + i, m + 1, i):
+            arr[j] = False
+# print(arr)
+# 얘는 없는 0을 False 처리
+arr[0] = False
+# 1은 소수가 애초에 아니니까 False 처리
+arr[1] = False
 
 for i in range(n, m + 1):
-    if isPrime(i):
+    if arr[i] == True and i >= n and i <= m:
         print(i)
