44b46b521ca19071c236744fc71733739c8c828e
moon_10**16 <108652903+kmk4162@users.noreply.github.com>
2023-01-04 16:57:28 +0900

    [Silver III] Title: 주유소, Time: 108 ms, Memory: 45988 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,16 +1,13 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
 road = list(map(int, input().split()))
 oil = list(map(int, input().split()))
 
-price = road[0] * oil[0] # 첫번째 가격
-
-# 가장 값이 싼 주유소
 min_oil = oil[0]
-for i in range(1, N - 1):
-    if min_oil > oil[i]:
+money = 0
+for i in range(N - 1):
+    if oil[i] > min_oil:
+        money += min_oil * road[i]
+    else:
+        money += oil[i] * road[i]
         min_oil = oil[i]
-    price += min_oil * road[i]
-print(price)
+print(money)
