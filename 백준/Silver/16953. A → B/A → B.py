2ddd56946a0d7da97e099ddf0256e85dab5eada5
moon_10**16 <108652903+kmk4162@users.noreply.github.com>
2023-01-04 17:31:37 +0900

    [Silver II] Title: A → B, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,45 +1,18 @@
 A, B = map(int, input().split())
-cnt = 0
-# 4 42
+cnt = 1
 while True:
-    if B == A:
-        cnt += 1
-        break
-    if A > B:
-        break
-    elif B % 2 == 0:
-        cnt += 1
-        B /= 2
-        continue
-    elif B % 10 == 1:
-        cnt += 1
-        B = B // 10
-        continue
+    cnt += 1
+    if int(str(B)[-1]) == 1:
+        B = int(str(B)[:-1])
     else:
+        if B % 2 == 0:
+            B //= 2
+        else:
+            cnt = -1
+            break
+    if B == A:
         break
-if A != B:
-    print(-1)
-else:
-    print(cnt)
- 
-# BFS 풀이
-from collections import deque
-
-A, B = map(int, input().split())
-# A에서 B까지
-# 연산 최솟값에 +1 출력, 못 만들면 -1
-# 100, 40021 경우
-
-def bfs(A, B):
-    queue = deque([(A, 1)])
-    while queue:
-        num, cnt = queue.popleft()
-        if num == B:
-            print(cnt)
-            return
-
-        for i in [num * 2, num * 10 + 1]:
-            if 1 <= i <= 1e9 and A <= i:
-                queue.append((i, cnt+1))
-    print(-1)
-bfs(A, B)
+    if B < A:
+        cnt = -1
+        break
+print(cnt)
