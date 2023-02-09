b04da6457adcaa8f304afaf69f975095fb7b9dfd
moon_10**16 <108652903+kmk4162@users.noreply.github.com>
2023-02-09 18:48:28 +0900

    [Gold V] Title: 두 용액, Time: 128 ms, Memory: 42176 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -7,23 +7,18 @@ arr.sort()
 
 left = 0
 right = N - 1
-answer = abs(arr[left] + arr[right])
-final = [arr[left], arr[right]]
+answer = 1e12
+answer_list = [0, 0]
 
 while left < right:
-    left_value = arr[left]
-    right_value = arr[right]
+    now_val = arr[left] + arr[right]
+    if abs(now_val) < answer:
+        answer = abs(now_val)
+        answer_list = [arr[left], arr[right]]
 
-    sum_value = left_value + right_value
-
-    # 더한 값이 이전보다 작다면 업데이트
-    if abs(sum_value) < answer: 
-        answer = abs(sum_value)
-        final = [left_value, right_value]
-        if answer == 0:
-            break
-    if sum_value < 0:
+    if now_val < 0:
         left += 1
     else:
         right -= 1
-print(*final)
+
+print(*answer_list)
